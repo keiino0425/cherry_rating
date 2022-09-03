9f23ed7ec82b8da284fc7279efbb4cf4ca633363
keiino0425 <keiino0425@gmail.com>
2022-09-04 01:54:11 +0900

    rubocop用にファイルを修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,5 +75,5 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   Rails.application.configure do
     config.hosts << "cherry-rating.herokuapp.com"
-end
+  end
 end
