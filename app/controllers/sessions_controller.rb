ba259b433d4ef4f488b3c9548f0deceb2a333863
keiino0425 <keiino0425@gmail.com>
2022-09-30 03:29:07 +0900

    sessionsコントローラーに機能を追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,16 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    SpreadsheetsImportJob.perform_now("1MDPFwFVab9_xnbPcMV2eMCd6PwVF_to6FMMytd54wwA", ["データ読取用!A:O"])
+    user = User.find_by(user_name: params[:session][:user_name])
+    if user && user.authenticate(params[:session][:password])
+    else
+      flash.now[:danger] = '名前かパスワードが正しくありません!'
+      render 'new'
+  end
+
+  def destroy
+  end
 end
