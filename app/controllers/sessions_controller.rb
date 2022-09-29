ac07404dc9aaa880b0a44c01777fc852e3593f22
keiino0425 <keiino0425@gmail.com>
2022-09-30 04:39:21 +0900

    rubocopの修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
     SpreadsheetsImportJob.perform_now("1MDPFwFVab9_xnbPcMV2eMCd6PwVF_to6FMMytd54wwA", ["データ読取用!A:O"])
     user = User.find_by(user_name: params[:session][:user_name])
     if user&.authenticate(params[:session][:password])
+      redirect_to user
     else
       flash.now[:danger] = '名前かパスワードが正しくありません!'
       render 'new'
